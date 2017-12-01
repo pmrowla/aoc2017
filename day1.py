7f0e7701c72325242cff8c31e9cfabf9bfda34f2
Peter Rowlands <peter@pmrowla.com>
2017-12-01 19:26:06 +0900

    day 1 solution

---
+++ day1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Advent of Code 2017 day 1 module."""
+from __future__ import division, print_function
+
+
+def sum_digits(digits, cmp_index=lambda x: x + 1):
+    """Sum all digits that match the another digit in the list.
+
+    Args:
+        digits (iterable): Input digit list.
+        cmp_index (callable, optional): Function to be used when determining
+            index of the digit to compare to the current digit. Defaults to
+            the next digit (j = i + 1).
+
+    Returns:
+        The sum of all digits that match the digit at index cmp_index().
+    """
+    total = 0
+    digits = list(map(int, digits))
+    for i in range(len(digits)):
+        j = cmp_index(i) % len(digits)
+        if digits[i] == digits[j]:
+            total += digits[i]
+    return total
+
+
+def part_one(digits):
+    return sum_digits(digits)
+
+
+def part_two(digits):
+    if len(digits) % 2 != 0:
+        raise(TypeError('list must contain an even number of digits'))
+    return sum_digits(digits, lambda x: x + len(digits) // 2)
+
+
+def main():
+    """Main entry point."""
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    puzzle_input = args.infile.read().strip()
+    print('Part one: {}'.format(part_one(puzzle_input)))
+    print('Part two: {}'.format(part_two(puzzle_input)))
+
+
+if __name__ == '__main__':
+    main()
